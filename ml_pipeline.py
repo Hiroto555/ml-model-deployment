d5dde2fd10f9a4f5829d12c61fb1565b07f3bb29
Hiroto Shimokawa <113822121+Hiroto555@users.noreply.github.com>
2023-02-04 12:41:45 -0800

    Add files via upload

---
+++ ml_pipeline.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  4 01:33:02 2023
+
+@author: Shgol
+"""
+
---
+++ use_model.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  4 12:25:25 2023
+
+@author: Shgol
+"""
+
